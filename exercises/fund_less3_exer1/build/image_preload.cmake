9b75849feefeffd45335a664adab98b75e313600
Leo Hofer <leo.hofer@hofernet.ch>
2022-12-30 17:37:15 +0100

    bkp

---
+++ exercises/fund_less3_exer1/build/image_preload.cmake
@@ -0,0 +1,6 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52833dk_nrf52833" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "/home/hfl2/Developer/nrf_academy/exercises/fund_less3_exer1/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "/home/hfl2/nordicsemi/toolchains/v2.2.0/usr/local/bin/python3.8" CACHE INTERNAL "NCS child image controlled")
---
+++ exercises/fund_less3_exer1/build/zephyr/zephyr.dts.d
@@ -0,0 +1,21 @@
+empty_file.o: /home/hfl2/nordicsemi/v2.2.0/zephyr/misc/empty_file.c \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/boards/arm/nrf52833dk_nrf52833/nrf52833dk_nrf52833.dts \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/dts/arm/nordic/nrf52833_qiaa.dtsi \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/dts/common/mem.h \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/dts/arm/nordic/nrf52833.dtsi \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/dts/arm/armv7-m.dtsi \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/dts/common/skeleton.dtsi \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/include/zephyr/dt-bindings/adc/nrf-adc.h \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/include/zephyr/sys/util_macro.h \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/include/zephyr/sys/util_internal.h \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/include/zephyr/sys/util_loops.h \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/dts/common/freq.h \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/dts/arm/nordic/override.dtsi \
+ /home/hfl2/nordicsemi/v2.2.0/zephyr/boards/arm/nrf52833dk_nrf52833/nrf52833dk_nrf52833-pinctrl.dtsi
