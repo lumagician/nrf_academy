ae31f048cae4494f4e2bb11bb6dfd87f4a08567a
Leo Hofer <leo.hofer@hofernet.ch>
2023-01-03 17:06:57 +0100

    4.1

--- exercises/fund_less4_exer1/src/main.c
+++ exercises/fund_less4_exer1/src/main.c
@@ -8,9 +8,12 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+
 /* STEP 6 - Include the header file of printk */
+#include <zephyr/sys/printk.h>
 
 /* STEP 8.1 - Define the macro MAX_NUMBER_FACT that represents the maximum number to calculate its factorial  */
+#define MAX_NUMBER_FACT 10
 
 #define SLEEP_TIME_MS   10*60*1000
 
@@ -21,10 +24,26 @@ static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 /* STEP 8.2 - Replace the button callback function */
-void button_pressed(const struct device *dev, struct gpio_callback *cb,
-		    uint32_t pins)
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-	gpio_pin_toggle_dt(&led);
+	int i;
+	int j;
+	long int factorial;
+
+	printk("CAlculating the factorials of mubers from 1 to %d\n\r", MAX_NUMBER_FACT);
+
+	for (i = 1; i < MAX_NUMBER_FACT; i++)
+	{
+		factorial = 1;
+
+		for(j = 1; j <= i; j++)
+		{
+			factorial = factorial * j;
+		}
+		printk("The factorial of %2d = %ld \n\r", i, factorial);
+	}
+
+	printk("_______________________________________________________\r");
 }
 
 static struct gpio_callback button_cb_data;
@@ -33,6 +52,7 @@ void main(void)
 {
 	int ret;
 	/* STEP 7 - Print a simple banner */
+	printk("nRF Academy \n\r");
 
 	/* Only checking one since led.port and button.port point to the same device, &gpio0 */
 	if (!device_is_ready(led.port)) {
